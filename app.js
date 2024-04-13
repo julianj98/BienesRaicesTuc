2d074b3e6ea11ce4d5ba9f921cabb3a55d7c0b2f
Julian Juarez <julianjuarez98.jj@gmail.com>
2024-04-13 02:24:36 -0300

    fix

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.use('/api',apiRoutes)
 
 
 
-const port = process.env.port || 3000;
+const port = process.env.port || 8080;
 app.listen(port,()=>{
     console.log(`Server is running in port ${port}`)
 })
