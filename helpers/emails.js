d9a912a10ac972e19b86172a1de3989f57432441
Julian Juarez <julianjuarez98.jj@gmail.com>
2025-06-06 23:52:11 -0300

    update servers

--- helpers/emails.js
+++ helpers/emails.js
@@ -5,6 +5,10 @@ const resend = new Resend(process.env.RESEND_API);
 const emailRegistro = async (datos) => {   
     const {email, nombre, token} = datos;
     
+    // Construir la URL de confirmación
+    const confirmarUrl = `${process.env.BACKEND_URL}/auth/confirmar/${token}`;
+    console.log('URL de confirmación:', confirmarUrl);
+    
     try {
         await resend.emails.send({
             from: 'BienesRaices <onboarding@resend.dev>',
@@ -14,7 +18,7 @@ const emailRegistro = async (datos) => {
                 <p>Hola ${nombre}, comprueba tu cuenta en BienesRaices.com</p>
                 
                 <p>Tu cuenta esta lista, solo debes confirmala con el siguiente enlace:
-                <a href="${process.env.BACKEND_URL}/auth/confirmar/${token}">Confirmar Cuenta</a></p>
+                <a href="${confirmarUrl}">Confirmar Cuenta</a></p>
 
                 <p>Si tu no creaste esta cuenta, puedes ignorar el mensaje</p>
             `
@@ -27,6 +31,10 @@ const emailRegistro = async (datos) => {
 const emailOlvidePassword = async (datos) => {   
     const {email, nombre, token} = datos;
     
+    // Construir la URL de recuperación
+    const recuperarUrl = `${process.env.BACKEND_URL}/auth/olvide-password/${token}`;
+    console.log('URL de recuperación:', recuperarUrl);
+    
     try {
         await resend.emails.send({
             from: 'BienesRaices <onboarding@resend.dev>',
@@ -36,7 +44,7 @@ const emailOlvidePassword = async (datos) => {
                 <p>Hola ${nombre}, has solicitado reestablecer tu password en BienesRaices.com</p>
                 
                 <p>Sigue el siguiente enlace para generar un password nuevo:
-                <a href="${process.env.BACKEND_URL}/auth/olvide-password/${token}">Reestablecer Password</a></p>
+                <a href="${recuperarUrl}">Reestablecer Password</a></p>
 
                 <p>Si tu no solicitaste el cambio de password, puedes ignorar el mensaje</p>
             `
